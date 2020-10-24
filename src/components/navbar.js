dc7ec61373cb5a030be65c05b93a608de3665a20
Alyssa Holland <alysam0071@gmail.com>
2020-10-23 23:00:46 -0400

    Add new logo to website

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,20 +1,24 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+import SVGSymbols from './SVGSymbols'
+
 const Navbar = (props) => {
   return (
     <nav
       id="header"
       className="fixed w-full z-30 top-0 text-white bg-white shadow"
     >
-      <div className="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-2">
+      <div className="w-full container flex flex-wrap items-center justify-between mt-0 py-2">
         {/**TODO: Logo goes here */}
         <div className="pl-4 flex items-center">
           <a
             className="toggleColour no-underline hover:no-underline font-bold text-2xl lg:text-4xl text-gray-800"
             href="#home"
           >
-            AH
+            <svg width="250" height="75" viewBox="0 0 398 188">
+              {SVGSymbols["logo"]["content"]}
+            </svg>
           </a>
         </div>
 
--- src/components/socialIcons.js
+++ src/components/socialIcons.js
@@ -8,6 +8,7 @@ import {
   faLinkedinIn,
 } from "@fortawesome/free-brands-svg-icons";
 import { faFileAlt } from "@fortawesome/free-solid-svg-icons";
+import SVGSymbols from './SVGSymbols'
 
 const SocialIcons = (props) => {
   return (
@@ -63,9 +64,7 @@ const SocialIcons = (props) => {
         target="_blank"
       >
         <svg className="w-8 h-8 text-gray-800 hover:text-gray-500" xmlns="http://www.w3.org/2000/svg" width="337" height="337" viewBox="0 0 337 337" fill="none">
-          <path fillRule="evenodd" clipRule="evenodd"
-                d="M23.155 112.598c-30.873 30.874-30.873 80.93 0 111.804l89.443 89.443c30.874 30.873 80.93 30.873 111.804 0l89.443-89.443c30.873-30.874 30.873-80.93 0-111.804l-89.443-89.443c-30.874-30.873-80.93-30.873-111.804 0l-89.443 89.443zm184.476 95.033c21.612-21.611 21.612-56.651 0-78.262-21.611-21.612-56.651-21.612-78.262 0-21.612 21.611-21.612 56.651 0 78.262 21.611 21.612 56.651 21.612 78.262 0z"
-                className="fill-current"/>
+          {SVGSymbols["hashnode"]["content"]}
         </svg>
       </a>
     </>
